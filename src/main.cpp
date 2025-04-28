9cf951aa1aad9022f60eabc2ed10003d3b10f9ff
Dulce Torres <adilenedulce33@gmail.com>
2025-04-27 18:10:21 -0700

    added bond order information to the class

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,18 @@ using json = nlohmann::json;
 
 struct atomic_orbital
 {
-    int atom_id;
+    double atom_id;
     std::string atom;
     std::string orbital_type;
     double atomic_number;
     arma::vec coordinates;
 
+};
+
+struct bond
+{
+    double atom_id; // the atom_id of the counterpart
+    double bond_order;
 };
 
 std::vector<atomic_orbital> create_atomic_orbitals(int atom_id, std::string atom, double atomic_number, arma::vec coordinates, bool hydrogen=false)
@@ -59,29 +65,74 @@ std::vector<atomic_orbital> create_atomic_orbitals(int atom_id, std::string atom
     return atomic_orbitals;
 }
 
+std::vector<bond> get_bond_vector(std::vector<double> aid1, std::vector<double> aid2, std::vector<double> bond_order, double atom_id)
+{
+    std::vector<bond> my_bonds;
+
+    for(int i=0; i < aid1.size(); i++)
+    {
+        double atom_id1 = aid1[i];
+        double atom_id2 = aid2[i];
+        if(atom_id1==atom_id)
+        {
+            bond bond_counterpart;
+            bond_counterpart.atom_id = aid2[i];
+            bond_counterpart.bond_order = bond_order[i];
+            my_bonds.push_back(bond_counterpart);
+        }
+        else if(atom_id2==atom_id)
+        {
+            bond bond_counterpart;
+            bond_counterpart.atom_id = aid1[i];
+            bond_counterpart.bond_order = bond_order[i];
+            my_bonds.push_back(bond_counterpart);
+        }
+    }
+    return my_bonds;
+}
+
 class Extended_Huckel
 {
     private:
     int N;
-    //std::vector<atomic_orbital> atomic_orbitals;
+    std::map<double, std::vector<bond>> bond_map; 
+    std::vector<atomic_orbital> atomic_orbitals;
     
     public:
-    std::vector<atomic_orbital> atomic_orbitals;
+    //std::vector<atomic_orbital> atomic_orbitals;
     Extended_Huckel(std::vector<std::vector<double>> compound_data)
     {
+        //coordinate information
         std::vector<double> atomic_numbers = compound_data[0];
         std::vector<double> x_coords = compound_data[1];
         std::vector<double> y_coords = compound_data[2];
         std::vector<double> z_coords = compound_data[3];
+        //bond information
+        std::vector<double> aid1 = compound_data[4];
+        std::vector<double> aid2 = compound_data[5];
+        std::vector<double> bond_orders = compound_data[5];
 
         int total_atoms = atomic_numbers.size();
+
+       
         for(int i=0; i < total_atoms; i++)
         {
             double atomic_number = atomic_numbers[i];
             std::string atom = element_map[atomic_number];
-            int atom_id = i;
+            double atom_id = i + 1.0;
             arma::vec coordinates = {x_coords[i], y_coords[i], z_coords[i]};
 
+            // create dictionary entry
+            bond_map[atom_id] = get_bond_vector(aid1, aid2, bond_orders, atom_id);
+            std::cout << "atom id: " << atom_id << std::endl;
+            std::vector<bond>  my_bonds = bond_map[atom_id];
+            std::cout << "MY BONDS: " << std::endl;
+            for(int j=0; j< my_bonds.size(); j++)
+            {
+                std::cout << my_bonds[j].atom_id << std::endl;
+            }
+
+            // create the atomic orbitals for atom
             if(atom=="H")
             {
                 std::vector<atomic_orbital> my_orbitals = create_atomic_orbitals(atom_id, atom, atomic_number, coordinates, true);
@@ -99,6 +150,8 @@ class Extended_Huckel
         }
 
         N = atomic_orbitals.size();
+
+
     }
 
     /*
@@ -161,9 +214,8 @@ std::vector<std::vector<double>> read_json(std::string filepath)
         std::cerr << "Parse error: " << e.what() << std::endl;
     }
     
-    // Extract elements and coordinates
-    std::vector<double> elements;
-    std::vector<double> x_coords, y_coords, z_coords;
+    // Extract elements, coordinates, and bond information 
+    std::vector<double> elements, x_coords, y_coords, z_coords, aid1, aid2, bond_orders;
     
     // Get the first compound (assuming single compound in array)
     auto& compound = data["PC_Compounds"][0];
@@ -184,8 +236,26 @@ std::vector<std::vector<double>> read_json(std::string filepath)
     for (auto& z : conformer["z"]) {
         z_coords.push_back(z);
     }
+    // Extract bond information
+    for (auto& a1 : compound["bonds"]["aid1"]) {
+        aid1.push_back(a1);
+    }
+    for (auto& a2 : compound["bonds"]["aid2"]) {
+         aid2.push_back(a2);
+    }
+    for (auto& order : compound["bonds"]["order"]) {
+        bond_orders.push_back(order);
+    }
 
-    std::vector<std::vector<double>>  compound_data = {elements, x_coords, y_coords, z_coords};
+    std::vector<std::vector<double>>  compound_data = {
+        elements, // index 0
+        x_coords, // index 1
+        y_coords, // index 2
+        z_coords, // index 3
+        aid1, // index 4
+        aid2, //index 5
+        bond_orders //index 6
+    };
     return compound_data;
 
 }
